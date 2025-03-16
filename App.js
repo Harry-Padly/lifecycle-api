807e22312c000aa5471ea831fd34c34c69ba2a3e
Harry-Padly <2310501012@mahasiswa.upnvj.ac.id>
2025-03-16 10:39:21 +0700

    DataFetcher.js dan App.js
    Nama : Harry Padly
    Nim : 2310501012
    Kelas : A
    
    - Menambahkan fungsi fetchData untuk mengambil data dari API
    - Memperbaiki struktur komponen di App.js agar sesuai serta dapat di run dan menampilkan hasil nya di localhost

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataFetcher from "./components/DataFetcher";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Lifecycle API Example</h1>
+      <DataFetcher />
+    </div>
+  );
+}
+
+export default App;
